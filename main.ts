50a29961d6dce6e1ef6428957b53b30930bdeeb2
inside-hakumai <hyme@insidehakumai.dev>
2025-05-05 19:50:04 +0900

    なんか動いた気がする

--- main.ts
+++ main.ts
@@ -1,134 +1,353 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginManifest } from "obsidian";
+import { v4 as uuidv4 } from "uuid";
+
+import "./styles.css";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+type TableId = string;
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class FullwidthTablePlugin extends Plugin {
+	private tableMap = new TableMap();
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+	private viewResizeObserver = new ResizeObserver((entries) => {
+		entries.forEach((entry) => {
+			const viewElement = entry.target;
+			const isValidElement =
+				viewElement instanceof HTMLElement &&
+				viewElement.tagName === "DIV" &&
+				viewElement.classList.contains("view-content");
+			if (!isValidElement) {
+				return;
 			}
+			this.storeViewSize(viewElement);
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+	});
+
+	private tableWidgetResizeObserver = new ResizeObserver((entries) => {
+		// entriesは配列で返ってくるが、実態としては1つの要素しか入り得ないはず
+		entries.forEach((entry) => {
+			const tableWidget = entry.target as HTMLElement;
+			this.setTableWrapperWidth(tableWidget);
+		});
+	});
+
+	private tableWidgetMutationObserver = new MutationObserver((mutations) => {
+		const mutated = new Set(
+			mutations
+				.map((mutation) => mutation.target as HTMLElement)
+				.filter((node) => node.classList.contains("cm-table-widget"))
+		);
+		mutated.forEach((tableWidget) => {
+			this.setTableWrapperWidth(tableWidget);
+		});
+	});
+
+	constructor(app: App, manifest: PluginManifest) {
+		super(app, manifest);
+
+		this.tableMap.addChangeEventListener(
+			"tableWrapperWidth",
+			(tableId, width) => {
+				console.log(
+					`tableWrapperWidth of ${tableId} updated: ${width}`
+				);
 			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+		this.tableMap.addChangeEventListener("leftGap", (tableId, left) => {
+			console.log(`leftGap of ${tableId} updated: ${left}`);
+			const tableWidgetElement = document.querySelector(
+				`div.cm-table-widget[data-plugin-fullwidth-table-id="${tableId}"]`
+			);
+			if (!tableWidgetElement) {
+				return;
 			}
+			if (!(tableWidgetElement instanceof HTMLElement)) {
+				throw new Error(
+					`tableWidgetElement is not an HTMLElement: ${tableId}`
+				);
+			}
+
+			tableWidgetElement.style.setProperty(
+				"--plugin-fullwidth-table-left-gap",
+				`${left}px`
+			);
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.tableMap.addChangeEventListener("viewWidth", (width) => {
+			document.body.style.setProperty(
+				"--plugin-fullwidth-table-view-width",
+				`${width}px`
+			);
+		});
+	}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+	override async onload() {
+		this.app.workspace.onLayoutReady(() => {
+			const viewContentElement = document.querySelector(".view-content");
+			console.log(viewContentElement);
+			if (
+				!viewContentElement ||
+				!(viewContentElement instanceof HTMLElement)
+			) {
+				throw new Error("viewContentElement not found");
+			}
+			this.viewResizeObserver.observe(viewContentElement);
+			this.storeViewSize(viewContentElement);
+
+			const cmContentElement = document.querySelector(".cm-content");
+			console.log(cmContentElement);
+			if (!cmContentElement) {
+				throw new Error("cmContentElement not found");
+			}
+
+			const tableInsertionObserver = new MutationObserver((mutations) => {
+				console.log(mutations);
+				const addedTableWidgets = new Set(
+					mutations
+						.filter((mutation) => mutation.type === "childList")
+						.flatMap((mutation) => Array.from(mutation.addedNodes))
+						.map((node) => node as HTMLElement)
+						.filter((node) =>
+							node.classList.contains("cm-table-widget")
+						)
+				);
+
+				console.log(addedTableWidgets);
+
+				addedTableWidgets.forEach((tableWidgetElement) =>
+					this.initTable(tableWidgetElement as HTMLElement)
+				);
+			});
+
+			tableInsertionObserver.observe(cmContentElement, {
+				childList: true,
+				subtree: false,
+				attributes: false,
+			});
+
+			this.init();
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this.registerMarkdownPostProcessor(
+
+		// 	(element: HTMLElement, context: MarkdownPostProcessorContext) => {
+		// 		console.debug(element, element.tagName, element.classList);
+
+		// 		const isTableWrapper =
+		// 			element.tagName === "DIV" &&
+		// 			element.classList.contains("el-table");
+		// 		if (isTableWrapper) {
+		// 			const tables = element.querySelectorAll("table");
+		// 			tables.forEach(this.processTable);
+		// 			return;
+		// 		}
+
+		// 		const isTableCellWrapper =
+		// 			element.tagName === "DIV" &&
+		// 			element.classList.contains("table-cell-wrapper");
+		// 		if (isTableCellWrapper) {
+		// 			const table = element.closest("table.table-editor");
+		// 			if (table && table instanceof HTMLTableElement) {
+		// 				this.processTable(table);
+		// 			}
+		// 		}
+		// 	}
+		// );
 	}
 
-	onunload() {
-
+	override onunload() {
+		// クリーンアップ処理があればここに記述
+		// 今回は registerMarkdownPostProcessor が自動で解除されるので基本不要
+		console.log("FullwidthTablePlugin: Unloading plugin");
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	private init() {
+		const tableWidgetElements = Array.from(
+			document.querySelectorAll("div.cm-table-widget")
+		);
+		console.log(
+			`Found ${tableWidgetElements.length} div.cm-table-widget elements`
+		);
+		tableWidgetElements.forEach((tableWidgetElement) =>
+			this.initTable(tableWidgetElement as HTMLElement)
+		);
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	private initTable(tableWidgetElement: HTMLElement) {
+		const tableId = uuidv4();
+		tableWidgetElement.dataset.pluginFullwidthTableId = tableId;
+
+		this.setTableWrapperWidth(tableWidgetElement);
+		this.tableWidgetResizeObserver.observe(tableWidgetElement);
+		this.tableWidgetMutationObserver.observe(tableWidgetElement, {
+			childList: true,
+			subtree: false,
+			attributes: false,
+		});
 	}
+
+	private setTableWrapperWidth(tableWidgetElement: HTMLElement) {
+		const tableId = tableWidgetElement.dataset.pluginFullwidthTableId;
+		if (!tableId) {
+			throw new Error("tableId not found in dataset");
+		}
+
+		const tableWrapperElement =
+			tableWidgetElement.querySelector("div.table-wrapper");
+		if (
+			!tableWrapperElement ||
+			!(tableWrapperElement instanceof HTMLElement)
+		) {
+			throw new Error("tableWrapperElement not found");
+		}
+
+		const width = tableWrapperElement.clientWidth;
+		this.tableMap.setTableWrapperWidth(tableId, width);
+	}
+
+	/**
+	 * @param viewElement div.view-content の要素
+	 */
+	private storeViewSize(viewElement: HTMLElement) {
+		const clientWidth = viewElement.clientWidth; // padding含む
+
+		const scroller = viewElement.querySelector("div.cm-scroller");
+		if (!scroller || !(scroller instanceof HTMLElement)) {
+			throw new Error("scroller not found");
+		}
+
+		console.log(scroller);
+		const computedStyle = window.getComputedStyle(scroller);
+		const padding =
+			parseFloat(computedStyle.paddingLeft) +
+			parseFloat(computedStyle.paddingRight);
+		const contentWidth = clientWidth - padding;
+
+		this.tableMap.setViewWidth(contentWidth);
+		console.debug(`Detected view width: ${contentWidth}`);
+	}
+
+	// private processTable(table: HTMLTableElement) {
+	// 	console.log("Found table element: ", table);
+	// 	// テーブル要素を直接含む親要素 (通常は <p> や <div>) を探す
+	// 	// もしくはテーブル自体にクラスを付与しても良いが、
+	// 	// スクロールコンテナとして親要素を使う方が一般的
+	// 	// ここでは、簡単のためテーブルの直接の親にクラスを付与する
+	// 	const parent = table.parentElement;
+	// 	console.log(parent, table.closest("div"));
+	// 	if (parent) {
+	// 		// 既にクラスが付与されているかチェック (重複付与防止)
+	// 		if (
+	// 			!parent.classList.contains(
+	// 				"fullwidth-table-container"
+	// 			)
+	// 		) {
+	// 			parent.classList.add("fullwidth-table-container");
+	// 			// デバッグ用にログ出力 (開発中は便利)
+	// 			console.log(
+	// 				"FullwidthTablePlugin: Applied class to table container"
+	// 			);
+	// 		}
+	// 	}
+	// }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+type ChangeEventListeners = {
+	tableWrapperWidth: ((tableId: TableId, width: number) => void)[];
+	leftGap: ((tableId: TableId, width: number) => void)[];
+	viewWidth: ((width: number) => void)[];
+};
+
+class TableMap {
+	/** Obsidianのファイル内容の表示領域（View）の横幅（padding除く） */
+	private viewWidth: number | undefined = undefined;
+	/** テーブル要素のラッパー要素（div.table-wrapper）の横幅 */
+	private tableWrapperWidth: Record<TableId, number> = {};
+	/** テーブル要素を通常の編集領域から左方向に何pxずらすか */
+	private leftGap: Record<TableId, number> = {};
+
+	private changeEventListeners: ChangeEventListeners = {
+		tableWrapperWidth: [],
+		leftGap: [],
+		viewWidth: [],
+	};
+
+	setTableWrapperWidth(tableId: TableId, width: number) {
+		const isValueChanged = this.tableWrapperWidth[tableId] !== width;
+
+		if (isValueChanged) {
+			this.tableWrapperWidth[tableId] = width;
+			for (const callback of this.changeEventListeners["tableWrapperWidth"]) {
+				callback(tableId, width);
+			}
+			this.calcLeftGap(tableId);
+		}
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	setViewWidth(width: number) {
+		this.viewWidth = width;
+		for (const callback of this.changeEventListeners["viewWidth"]) {
+			callback(width);
+		}
+
+		for (const tableId of Object.keys(this.leftGap)) {
+			this.calcLeftGap(tableId);
+		}
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	/**
+	 * TableWidget要素を左方向に何pxずらすかを計算する
+	 *
+	 * - TableWrapper要素の幅が編集領域の1行の横幅よりも小さい場合
+	 *     => ずらす必要はないので0を返す
+	 * - TableWrapper要素の幅が編集領域の1行の横幅よりも大きく、viewの横幅よりも小さい場合
+	 *     => TableWrapper要素の横幅とviewの横幅の差の半分だけずらす
+	 * - TableWrapper要素の幅がviewの横幅よりも大きい場合
+	 *     => viewの横幅いっぱいに表示し、それよりも超過する分はスクロールできるようにするので、viewの横幅と編集領域の横幅の差の半分だけずらす
+	 */
+	private calcLeftGap(tableId: TableId) {
+		const tableWrapperWidth = this.tableWrapperWidth[tableId];
+		if (tableWrapperWidth === undefined) {
+			throw new Error(`tableWrapperWidth not found in tableMap: ${tableId}`);
+		}
+		if (this.viewWidth === undefined) {
+			throw new Error(`viewWidth is not initialized`);
+		}
+
+		const lineWidth = document.querySelector(
+			".cm-contentContainer"
+		)?.clientWidth;
+		if (lineWidth === undefined) {
+			throw new Error(`lineWidth is not initialized`);
+		}
+
+		let newLeftGap: number;
+		if (tableWrapperWidth < lineWidth) {
+			newLeftGap = 0;
+		} else if (
+			lineWidth < tableWrapperWidth &&
+			tableWrapperWidth < this.viewWidth
+		) {
+			newLeftGap = (tableWrapperWidth - lineWidth) / 2;
+		} else {
+			newLeftGap = (this.viewWidth - lineWidth) / 2;
+		}
+
+		this.leftGap[tableId] = newLeftGap;
+		for (const callback of this.changeEventListeners["leftGap"]) {
+			callback(tableId, newLeftGap);
+		}
 	}
-}
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	addChangeEventListener<T extends keyof ChangeEventListeners>(
+		event: T,
+		callback: ChangeEventListeners[T][number]
+	) {
+		if (!this.changeEventListeners[event]) {
+			this.changeEventListeners[event] = [];
+		}
+		this.changeEventListeners[event].push(callback as any);
 	}
 }
